fb66ef4d2b4c4e3401f35f19daa72e9180685ac0
胥天昊 <1499487526@qq.com>
2023-07-13 18:53:04 +0800

    m

--- react_staging/16_src_向路由组件传递params参数/pages/Home/Message/index.jsx
+++ react_staging/16_src_向路由组件传递params参数/pages/Home/Message/index.jsx
@@ -1,35 +1,36 @@
-import React, { Component } from 'react'
-import {Link,Route} from 'react-router-dom'
-import Detail from './Detail'
+import React, { Component } from "react";
+import { Link, Route } from "react-router-dom";
+import Detail from "./Detail";
 
 export default class Message extends Component {
-	state = {
-		messageArr:[
-			{id:'01',title:'消息1'},
-			{id:'02',title:'消息2'},
-			{id:'03',title:'消息3'},
-		]
-	}
-	render() {
-		const {messageArr} = this.state
-		return (
-			<div>
-				<ul>
-					{
-						messageArr.map((msgObj)=>{
-							return (
-								<li key={msgObj.id}>
-									{/* 向路由组件传递params参数 */}
-									<Link to={`/home/message/detail/${msgObj.id}/${msgObj.title}`}>{msgObj.title}</Link>
-								</li>
-							)
-						})
-					}
-				</ul>
-				<hr/>
-				{/* 声明接收params参数 */}
-				<Route path="/home/message/detail/:id/:title" component={Detail}/>
-			</div>
-		)
-	}
+  state = {
+    messageArr: [
+      { id: "01", title: "消息1" },
+      { id: "02", title: "消息2" },
+      { id: "03", title: "消息3" },
+    ],
+  };
+  render() {
+    const { messageArr } = this.state;
+    return (
+      <div>
+        <ul>
+          {messageArr.map((msgObj) => {
+            return (
+              <li key={msgObj.id}>
+                {/* 向路由组件传递params参数 */}
+                <Link to={`/home/message/detail/${msgObj.id}/${msgObj.title}`}>
+                  {msgObj.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+        <hr />
+        {/* 声明接收params参数 */}
+        {/* 下面这个参数可以在子组件中以props的形式接收，props直接给封装好了 */}
+        <Route path="/home/message/detail/:id/:title" component={Detail} />
+      </div>
+    );
+  }
 }
--- react_staging/README.md
+++ react_staging/README.md
@@ -104,20 +104,20 @@
 
 ## 十一、向路由组件传递参数
 
-    			1.params参数
-    						路由链接(携带参数)：<Link to='/demo/test/tom/18'}>详情</Link>
-    						注册路由(声明接收)：<Route path="/demo/test/:name/:age" component={Test}/>
-    						接收参数：this.props.match.params
-    			2.search参数
-    						路由链接(携带参数)：<Link to='/demo/test?name=tom&age=18'}>详情</Link>
-    						注册路由(无需声明，正常注册即可)：<Route path="/demo/test" component={Test}/>
-    						接收参数：this.props.location.search
-    						备注：获取到的search是urlencoded编码字符串，需要借助querystring解析
-    			3.state参数
-    						路由链接(携带参数)：<Link to={{pathname:'/demo/test',state:{name:'tom',age:18}}}>详情</Link>
-    						注册路由(无需声明，正常注册即可)：<Route path="/demo/test" component={Test}/>
-    						接收参数：this.props.location.state
-    						备注：刷新也可以保留住参数
+1.params 参数
+路由链接(携带参数)：<Link to='/demo/test/tom/18'}>详情</Link>
+注册路由(声明接收)：<Route path="/demo/test/:name/:age" component={Test}/>
+接收参数：this.props.match.params
+2.search 参数
+路由链接(携带参数)：<Link to='/demo/test?name=tom&age=18'}>详情</Link>
+注册路由(无需声明，正常注册即可)：<Route path="/demo/test" component={Test}/>
+接收参数：this.props.location.search
+备注：获取到的 search 是 urlencoded 编码字符串，需要借助 querystring 解析
+3.state 参数
+路由链接(携带参数)：<Link to={{pathname:'/demo/test',state:{name:'tom',age:18}}}>详情</Link>
+注册路由(无需声明，正常注册即可)：<Route path="/demo/test" component={Test}/>
+接收参数：this.props.location.state
+备注：刷新也可以保留住参数
 
 ## 十二、编程式路由导航
 
