9f709f262eb9d4f7530028df704581f52eb39bee
胥天昊 <1499487526@qq.com>
2023-07-15 13:35:26 +0800

    m

--- react_extension/src/components/1_setState/index.jsx
+++ react_extension/src/components/1_setState/index.jsx
@@ -1,29 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Demo extends Component {
+  state = { count: 0 };
 
-	state = {count:0}
+  add = () => {
+    //对象式的setState
+    //1.获取原来的count值
+    const { count } = this.state;
+    //2.更新状态
+    this.setState({ count: count + 1 }, () => {
+      console.log(this.state.count); //这里会获得更新后的值，更新后会调用这段代码
+    });
+    console.log("12行的输出", this.state.count); //还是更新前的值，原因是react更新state是异步的，所以这句代码会比更新先执行。
 
-	add = ()=>{
-		//对象式的setState
-		/* //1.获取原来的count值
-		const {count} = this.state
-		//2.更新状态
-		this.setState({count:count+1},()=>{
-			console.log(this.state.count);
-		})
-		//console.log('12行的输出',this.state.count); //0 */
+    //函数式的setState
+    this.setState((state) => ({ count: state.count + 1 })); //这里当然也可以传一个回调。也就是第二个参数，和上面的一样
+  };
 
-		//函数式的setState
-		this.setState( state => ({count:state.count+1}))
-	}
-
-	render() {
-		return (
-			<div>
-				<h1>当前求和为：{this.state.count}</h1>
-				<button onClick={this.add}>点我+1</button>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <h1>当前求和为：{this.state.count}</h1>
+        <button onClick={this.add}>点我+1</button>
+      </div>
+    );
+  }
 }
--- react_extension/src/components/2_lazyLoad/index.jsx
+++ react_extension/src/components/2_lazyLoad/index.jsx
@@ -1,43 +1,56 @@
-import React, { Component,lazy,Suspense} from 'react'
-import {NavLink,Route} from 'react-router-dom'
+import React, { Component, lazy, Suspense } from "react";
+import { NavLink, Route } from "react-router-dom";
 
+// 原始的引入，这里会直接引入进去，没有实现懒加载
 // import Home from './Home'
 // import About from './About'
 
-import Loading from './Loading'
-const Home = lazy(()=> import('./Home') )
-const About = lazy(()=> import('./About'))
+// Loading组件是下面某个组件正在加载中的时候呈现的，所以需要直接引入
+import Loading from "./Loading";
+// 这两个可以用懒加载
+const Home = lazy(() => import("./Home"));
+const About = lazy(() => import("./About"));
 
 export default class Demo extends Component {
-	render() {
-		return (
-			<div>
-				<div className="row">
-					<div className="col-xs-offset-2 col-xs-8">
-						<div className="page-header"><h2>React Router Demo</h2></div>
-					</div>
-				</div>
-				<div className="row">
-					<div className="col-xs-2 col-xs-offset-2">
-						<div className="list-group">
-							{/* 在React中靠路由链接实现切换组件--编写路由链接 */}
-							<NavLink className="list-group-item" to="/about">About</NavLink>
-							<NavLink className="list-group-item" to="/home">Home</NavLink>
-						</div>
-					</div>
-					<div className="col-xs-6">
-						<div className="panel">
-							<div className="panel-body">
-								<Suspense fallback={<Loading/>}>
-									{/* 注册路由 */}
-									<Route path="/about" component={About}/>
-									<Route path="/home" component={Home}/>
-								</Suspense>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header">
+              <h2>React Router Demo</h2>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              {/* 在React中靠路由链接实现切换组件--编写路由链接 */}
+              <NavLink className="list-group-item" to="/about">
+                About
+              </NavLink>
+              <NavLink className="list-group-item" to="/home">
+                Home
+              </NavLink>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                {/* 
+                  这里必须要指定这个标签，意思是下面路由组件正在加载的时候显示的组件
+                  如果没有这个标签的话会报错
+                */}
+                <Suspense fallback={<Loading />}>
+                  {/* 注册路由 */}
+                  <Route path="/about" component={About} />
+                  <Route path="/home" component={Home} />
+                </Suspense>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
